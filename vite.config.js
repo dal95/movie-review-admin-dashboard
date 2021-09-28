06b1175c6c60b4a635acafc57faef1394233a949
Afdallah <afdallah.war@gmail.com>
2021-09-29 01:13:15 +0700

    Add vite svg plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const reactSvgPlugin = require('vite-plugin-react-svg')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactSvgPlugin()],
   define: {
     'process.env': process.env
   }
